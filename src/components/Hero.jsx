2fdd96a2dcd00d47f3aa431985ed98aedc73c01a
swaraj-tech <yadavsushma313@gmail.com>
2025-08-16 15:14:12 +0530

    Implemented hero scroll animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,16 @@
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
-import React from 'react'
+import { useRef } from 'react'
 import { SplitText, ScrollTrigger } from "gsap/all";
+import { useMediaQuery } from 'react-responsive';
 gsap.registerPlugin(SplitText, ScrollTrigger);
 
 
 const Hero = () => {
+    const videoRef = useRef();
+    const videoTimelineRef = useRef()
+    const isMobile = useMediaQuery({ maxWidth: 767 })
+
     useGSAP(() => {
         const heroSplit = new SplitText('.title', { type: 'chars,words' });
         const paragraphSplit = new SplitText('.subtitle', { type: 'lines' });
@@ -41,45 +46,77 @@ const Hero = () => {
             ease: "sine.inOut",
         });
 
+        const startValue = isMobile ? 'top 50%' : 'center 60%';
+        const endValue = isMobile ? '120% top' : 'bottom top';
+
+        //Video animation timeline
+       const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: 'video',
+                start: startValue,
+                end: endValue,
+                scrub: true,
+                pin:true,
+               
+            }
+        })
+
+        videoRef.current.onloadedmetadata = ()=>{
+            tl.to(videoRef.current,{
+                currentTime: videoRef.current.duration
+            })
+
+        }
+    
 
 
     }, []);
-    return (
-        <>
-            <section id="hero" className='noisy' >
-                <h1 className='title'>MOJITO</h1>
+return (
+    <>
+        <section id="hero" className='noisy' >
+            <h1 className='title'>MOJITO</h1>
 
-                <img
-                    src='/images/hero-left-leaf.png'
-                    alt='left-leaf'
-                    className='left-leaf' />
+            <img
+                src='/images/hero-left-leaf.png'
+                alt='left-leaf'
+                className='left-leaf' />
 
-                <img
-                    src='/images/hero-right-leaf.png'
-                    alt='right-leaf'
-                    className='right-leaf' />
+            <img
+                src='/images/hero-right-leaf.png'
+                alt='right-leaf'
+                className='right-leaf' />
 
-                <div className='body'>
-                    <div className='content'>
-                        <div className="space-y-5 hidden md:block">
-                            <p>Cool. Crisp. Classic.</p>
-                            <p className="subtitle">
-                                Sip the Spirit <br /> of Summer
-                            </p>
-                        </div>
+            <div className='body'>
+                <div className='content'>
+                    <div className="space-y-5 hidden md:block">
+                        <p>Cool. Crisp. Classic.</p>
+                        <p className="subtitle">
+                            Sip the Spirit <br /> of Summer
+                        </p>
+                    </div>
 
-                        <div className='view-cocktails'>
-                            <p className='subtitle'>
-                                Every cocktail on our menu is a blend of premium ingredients,
-                                creative flair, and timeless recipes - designed to delight your senses.
-                            </p>
-                            <a href='#cocktails'>View Cocktails</a>
-                        </div>
+                    <div className='view-cocktails'>
+                        <p className='subtitle'>
+                            Every cocktail on our menu is a blend of premium ingredients,
+                            creative flair, and timeless recipes - designed to delight your senses.
+                        </p>
+                        <a href='#cocktails'>View Cocktails</a>
                     </div>
                 </div>
-            </section>
-        </>
-    )
+            </div>
+        </section>
+        <div className='video ansolute inset-0'>
+            <video
+                ref={videoRef}
+                src='/videos/output.mp4'
+                muted
+                playsInline
+                preload='auto'
+            />
+
+        </div>
+    </>
+)
 }
 
 export default Hero
