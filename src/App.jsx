1266634d204c9fb74cfe6538aa47110aa60cb5f3
swaraj-tech <yadavsushma313@gmail.com>
2025-08-18 15:33:38 +0530

    Implemented Contact Us Section

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import About from './components/About';
 import Cocktails from './components/Cocktails';
 import Art from './components/Art';
 import Menu from './components/Menu';
+import Contact from './components/Contact';
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
@@ -18,6 +19,7 @@ const App = () => {
             <About/>
             <Art/>
             <Menu/>
+            <Contact/>
         </main>
     )
 }
